07f609fafa06e20fbc569860c1befbe367efb266
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-08-05 10:25:59 +0700

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"; //ต้อง import ทุกครั้ง
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className="title"> React & React Native</div>
+    </> //เรียกการตกแต่งมาจาก style.css
+  );
+}
+
+export default App; // นำออกมาใช้งาน

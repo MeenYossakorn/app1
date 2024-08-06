a9debc70ac0119277088b0f2fc53a3e94826bf70
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-08-06 13:52:37 +0700

    ๊Update App.js
    ๊Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react"; //ต้อง import ทุกครั้ง
-import {Header,Content,Footer} from './func-components'
-import Calender from './class-component'
+import { Header, Content, Footer } from "./func-components";
+import Calender from "./class-component";
 
 function App() {
   return (
     <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>   
+      <Header />
+      <p>
+        <center>
+          <Calender />
+        </center>
+      </p>
+      <Content />
+      <Footer />
     </>
-  )
-}//เรียกการตกแต่งมาจาก style.css
+  );
+} //เรียกการตกแต่งมาจาก style.css
 
-export default App // นำออกมาใช้งาน
+export default App; // นำออกมาใช้งาน
